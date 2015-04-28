49936fd3b8029b4d688ad7f0255c2797f0ef04a0
Fernando <ftobar@deviget.com>
2015-04-28 19:43:03 -0300

    Patch for try to fix Rails 4 oauth2 handler issue

--- ads_common/lib/ads_common/auth/oauth2_handler.rb
+++ ads_common/lib/ads_common/auth/oauth2_handler.rb
@@ -80,6 +80,7 @@ module AdsCommon
       # Overrides base get_token method to account for the token expiration.
       def get_token(credentials = nil)
         token = super(credentials)
+        @client.issued_at = Time.parse(@client.issued_at) if @client.issued_at.class.name == 'String'
         token = refresh_token! if !@client.nil? && @client.expired?
         return token
       end
